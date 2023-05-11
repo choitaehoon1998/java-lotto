b650d3e692850cada9e0d4fea614b3c6f025bf62
choitaehoon1998 <hugebigdream@gmail.com>
2023-05-11 11:16:33 +0900

    feat : 수익률 계산하기

---
+++ src/main/java/lotto/ProfitRateCalculator.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ProfitRateCalculator {
+
+    public static String calculateProfitRate(int money, Long winningMoney) {
+        BigDecimal decimal = BigDecimal.valueOf((double) winningMoney / money).setScale(2, RoundingMode.DOWN);
+        return String.format("%.2f", decimal);
+    }
+}
---
+++ src/test/java/lotto/ProfitRateCalculatorTest.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfitRateCalculatorTest {
+
+    @Test
+    @DisplayName("정상적으로 수익률이 계산되는지")
+    void calculate() {
+        String profitRate = ProfitRateCalculator.calculateProfitRate(14000, 5000L);
+
+        assertEquals("0.35", profitRate);
+    }
+}
